72cf4a81b689f41ec7c3c844d0a24809c8f820f6
MambaBusinka <ban123fav@gmail.com>
2022-11-05 15:57:16 +1000

    Доделано

--- Wpf_Yshakow_PR7INS/MainWindow.xaml.cs
+++ Wpf_Yshakow_PR7INS/MainWindow.xaml.cs
@@ -25,6 +25,9 @@ namespace Wpf_Yshakow_PR7INS
             InitializeComponent();
         }
 
+        int a = 0;
+        int b;
+
         private void P1_MouseDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Это классический вариант итальянской пиццы на тонком тесте с минимальным набором ингредиентов. В её состав входят томаты, моцарелла, базилик и оливковое масло.  А знаешь ли ты историю появления этой пиццы? На торжестве в честь дня рождения королевы Италии Маргариты Савойской, была подана пицца в виде итальянского флага. Королева оценила оригинальность своих поваров, и повелела назвать эту пиццу в честь себя. Так и повелось.", "Описание Маргариты:");
@@ -87,62 +90,98 @@ namespace Wpf_Yshakow_PR7INS
 
         private void b1_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P1.Text + " " + C1.Text + Environment.NewLine;
+            b = b + 400;
+            itog.Text = b.ToString();
         }
 
         private void b2_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P2.Text + " " + C2.Text + Environment.NewLine;
+            b = b + 450;
+            itog.Text = b.ToString();
         }
 
         private void b3_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P3.Text + " " + C3.Text + Environment.NewLine;
+            b = b + 420;
+            itog.Text = b.ToString();
         }
 
         private void b4_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P4.Text + " " + C4.Text + Environment.NewLine;
+            b = b + 200;
+            itog.Text = b.ToString();
         }
 
         private void b5_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P5.Text + " " + C5.Text + Environment.NewLine;
+            b = b + 500;
+            itog.Text = b.ToString();
         }
 
         private void b6_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P6.Text + " " + C6.Text + Environment.NewLine;
+            b = b + 150;
+            itog.Text = b.ToString();
         }
 
         private void b7_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P7.Text + " " + C7.Text + Environment.NewLine;
+            b = b + 100;
+            itog.Text = b.ToString();
         }
 
         private void b8_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P8.Text + " " + C8.Text + Environment.NewLine;
+            b = b + 100;
+            itog.Text = b.ToString();
         }
 
         private void b9_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P9.Text + " " + C9.Text + Environment.NewLine;
+            b = b + 150;
+            itog.Text = b.ToString();
         }
 
         private void b10_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P10.Text + " " + C10.Text + Environment.NewLine;
+            b = b + 110;
+            itog.Text = b.ToString();
         }
 
         private void b11_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P11.Text + " " + C11.Text + Environment.NewLine;
+            b = b + 50;
+            itog.Text = b.ToString();
         }
 
         private void b12_Click(object sender, RoutedEventArgs e)
         {
-
+            a = a + 1;
+            Zak.Text = Zak.Text += a + " " + P12.Text + " " + C12.Text + Environment.NewLine;
+            b = b + 75;
+            itog.Text = b.ToString();
         }
     }
 }
